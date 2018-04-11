1a359d1f2e17e8eb0bfe0b1a85a0961cd530f3ea
Anitian Sherlock <anitiansherlock@users.noreply.github.com>
2018-04-11 12:00:01 -0700

    Add files via upload
    updated powershell script install to start the winlogbeat service after install

---
+++ install-service-winlogbeat.ps1
@@ -0,0 +1,17 @@
+# delete service if it already exists
+if (Get-Service winlogbeat -ErrorAction SilentlyContinue) {
+  $service = Get-WmiObject -Class Win32_Service -Filter "name='winlogbeat'"
+  $service.StopService()
+  Start-Sleep -s 1
+  $service.delete()
+}
+
+$workdir = Split-Path $MyInvocation.MyCommand.Path
+
+# create new service
+New-Service -name winlogbeat `
+  -displayName winlogbeat `
+  -binaryPathName "`"$workdir\\winlogbeat.exe`" -c `"$workdir\\winlogbeat.yml`" -path.home `"$workdir`" -path.data `"C:\\ProgramData\\winlogbeat`" -path.logs `"C:\\ProgramData\\winlogbeat\logs`""
+
+# start service (this is an addition to the original winlogbeat install srcipt)
+Restart-Service -DisplayName winlogbeat
